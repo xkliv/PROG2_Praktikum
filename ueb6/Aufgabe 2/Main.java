a46f690193e22308f8f1a42c30c1401f9711a237
xkliv <hamodek64@gmail.com>
2026-06-07 15:50:44 +0200

    last

--- ueb6/Aufgabe 2/Main.java
+++ ueb6/Aufgabe 2/Main.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
 
-        Function<Integer, Integer> quersumme = (num) -> {
+        Function<Integer, Integer> quersumme = num -> {
             num = Math.abs(num);
 
             int summe = 0;
@@ -16,12 +16,11 @@ public class Main {
             return summe;
         };
 
-        Function<Integer, Integer> letzteZiffer = (num) -> {
+        Function<Integer, Integer> letzteZiffer = num -> num % 10;
 
-            return num % 10;
-        };
+        Function<Integer, Integer> letzteBeidenZiffen = num -> num % 100;
 
-
+        System.out.println(letzteBeidenZiffen.apply(13344));
         System.out.println(letzteZiffer.apply(756));
         System.out.println(quersumme.apply(456));
     }
